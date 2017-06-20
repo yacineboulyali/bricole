c2ce2682a27df8db4ede6231ee4bc664c15b542e
Yacine Boulyali <yacineboulyalis@gmail.com>
2017-06-20 03:16:16 +0100

    conver profile_activity to constraint layout

--- app/src/main/java/com/yacineboulyali/bricole/ProfileActivity.java
+++ app/src/main/java/com/yacineboulyali/bricole/ProfileActivity.java
@@ -1,26 +1,16 @@
 package com.yacineboulyali.bricole;
 
-import android.content.Context;
-import android.content.Intent;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
-import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
-import android.view.ViewGroup;
-import android.widget.AdapterView;
-import android.widget.ArrayAdapter;
-import android.widget.BaseAdapter;
 import android.widget.GridView;
-import android.widget.ImageView;
-import android.widget.Toast;
+
+/**
+ * Created by Yacine on 19/06/2017.
+ */
 
 public class ProfileActivity extends AppCompatActivity {
-
 
     GridView gridView;
 
@@ -30,41 +20,41 @@ public class ProfileActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile);
 
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
+        //Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        //setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
-
-
-        gridView = (GridView) findViewById(R.id.gridview);
-        gridView.setAdapter(new GridImageAdapter(this));
-
-        gridView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-            @Override
-            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-
-                Intent i = new Intent(getApplicationContext(),FulImageActivity.class);
-                i.putExtra("id",position);
-                startActivity(i);
+        //FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        //fab.setOnClickListener(new View.OnClickListener() {
+           // @Override
+            //public void onClick(View view) {
+          //      Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+            //            .setAction("Action", null).show();
+        //    }
+        //});
 
 
+      //  gridView = (GridView) findViewById(R.id.gridview);
+        //  gridView.setAdapter(new GridImageAdapter(this));
 
-                }
-        });
+//        gridView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+        //          @Override
+        //  public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+//
+        //              Intent i = new Intent(getApplicationContext(),FulImageActivity.class);
+        //      i.putExtra("id",position);
+        //      startActivity(i);
+
+
+
+        //  }
+        //});
 
     }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_map, menu);
+        getMenuInflater().inflate(R.menu.menu_profile, menu);
         return true;
     }
 
@@ -75,10 +65,7 @@ public class ProfileActivity extends AppCompatActivity {
         // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
         //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            Intent intent = new Intent(this, ProfileActivity.class);
-            this.startActivity(intent);
-        }
+
         return super.onOptionsItemSelected(item);
     }
 }
